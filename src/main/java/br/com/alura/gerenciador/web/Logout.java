94103345ec982004f73aeffccd2f247282c4e5ae
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-06-10 10:02:48 -0300

    Aula 09 Atividade 01: Redirecionamento no client e no server side

--- src/main/java/br/com/alura/gerenciador/web/Logout.java
+++ src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -3,6 +3,7 @@ package br.com.alura.gerenciador.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -18,10 +19,11 @@ public class Logout extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getSession().removeAttribute("usuario.logado");
-
-		PrintWriter writer = resp.getWriter();
-
-		writer.println("<html><body>Deslogado com sucesso!</body></html>");
+		
+		// resp.sendRedirect("WEB-INF/paginas/logout.html");
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/paginas/logout.html");
+		dispatcher.forward(req, resp);
 	}
 
 }
